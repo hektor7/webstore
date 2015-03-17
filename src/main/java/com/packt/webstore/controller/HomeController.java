ef3693bb63117621902dc10d803904d968b1f6e2
Héctor Vizcaíno Cortegano <hektor7@gmail.com>
2015-03-17 19:24:06 +0100

    Redirect vs Forward explanation.

--- src/main/java/com/packt/webstore/controller/HomeController.java
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -12,6 +12,19 @@ public class HomeController {
 		model.addAttribute("greeting", "Welcome to Web Store!");
 		model.addAttribute("tagline", "The one and only amazing webstore");
 
+		return "forward:/welcome/greeting";
+		
+		// REDIRECT vs FORWARD
+		//If we use forward prefix, the process will happen in the server side so
+		//in this way we can refer to greeting and tagline attributes.
+		//However if we use redirect prefix, this cause another request (and we haven't
+		//sent any arguments in the second request) and this cause we
+		//can't refer to these attributes because they don't exist.
+
+	}
+
+	@RequestMapping("/welcome/greeting")
+	public String greeting() {
 		return "welcome";
 	}
 }
